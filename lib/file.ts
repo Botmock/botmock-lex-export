faeea3dd444983c1764edacee1b6c7e63615a719
Kevin Donahue <nonnontrivial@gmail.com>
2019-10-30 09:53:39 -0400

    add static properties to file writer

--- lib/file.ts
+++ lib/file.ts
@@ -1,19 +1,29 @@
-import { default as findEntity } from "@botmock-api/entity-map";
+// import { default as findEntity } from "@botmock-api/entity-map";
+// import { wrapEntitiesWithChar } from "@botmock-api/text";
 import * as flow from "@botmock-api/flow";
 import { writeJson } from "fs-extra";
 import { join } from "path";
 import { EOL } from "os";
 
-export type LexResource = {};
-export type ProjectData<T> = T extends Promise<infer K> ? K : any;
+namespace Lex {
+  export interface Resource {};
+  export type Intents = unknown[];
+  export type Slots = unknown[];
+}
 
-enum ContentType {
+enum ContentTypes {
   text = "PlainText",
 }
 
 enum ObfuscationSettings {
-  none = "NONE"
+  none = "NONE",
 }
+
+enum ValueSelectionStrategies {
+  original = "ORIGINAL_VALUE",
+}
+
+export type ProjectData<T> = T extends Promise<infer K> ? K : any;
 
 interface IConfig {
   readonly outputDirectory: string;
@@ -21,12 +31,13 @@ interface IConfig {
 }
 
 export default class FileWriter extends flow.AbstractProject {
-  static fallbackIntent = "I didn't understand you, what would you like me to do?";
-  static quitIntent = "Sorry, I am not able to assist at this time";
+  static clarificationPrompt = "I didn't understand you, what would you like me to do?";
+  static abortStatement = "Sorry, I am not able to assist at this time";
   static voiceId = "Salli";
   static locale = "en-US";
   static version = "1";
   static sessionTTLSecs = 800;
+  static isChildDirected = false;
   private outputDirectory: string;
 /**
  * Creates new instance of FileWriter class
@@ -46,34 +57,55 @@ export default class FileWriter extends flow.AbstractProject {
     return text.replace(disallowedCharactersRegex, "");
   }
   /**
+   * Genereates lex intents from the original project
+   */
+  private generateSlotTypesForProject(): Lex.Slots {
+    return [];
+  }
+  /**
+   * Genereates lex intents from the original project
+   * 
+   * @remarks ..
+   */
+  private generateIntentsForProject(): Lex.Intents {
+    return [];
+  }
+  /**
    * Generates resource object from project data
    * @returns object able to be serialized and written
    */
-  private generateResourceDataForProject(): LexResource {
+  private generateResourceDataForProject(): Lex.Resource {
     const { name } = this.projectData.project;
     const metadata = {
       schemaVersion: "1.0",
       importType: "LEX",
       importFormat: "JSON",
     };
+    const description = `generated ${new Date().toLocaleDateString()}`;
     return {
       metadata,
       resource: {
         name: this.sanitizeText(name),
         version: FileWriter.version,
-        intents: [],
-        slotTypes: [],
-        childDirected: false,
+        intents: this.generateIntentsForProject(),
+        slotTypes: this.generateSlotTypesForProject(),
         voiceId: FileWriter.voiceId,
+        childDirected: FileWriter.isChildDirected,
         locale: FileWriter.locale,
         idleSessionTTLInSeconds: FileWriter.sessionTTLSecs,
-        description: name,
+        description,
         clarificationPrompt: {
-          messages: [],
+          messages: [{
+            contentType: ContentTypes.text,
+            content: FileWriter.clarificationPrompt,
+          }],
           maxAttempts: 2,
         },
         abortStatement: {
-          messages: [],
+          messages: [{
+            contentType: ContentTypes.text,
+            content: FileWriter.abortStatement,
+          }],
         },
       }
     }
